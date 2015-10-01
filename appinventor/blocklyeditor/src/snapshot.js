d760fa49ce44e1bcd82342d4ed23b214086586be
Mark Sherman <msherman@cs.uml.edu>
2015-10-01 19:11:22 -0400

    Snapshots use an XhrIOPool for multiple, rapid requests.
    Change-Id: I4f31408e70b52ddc5c963ac01d70191124d0605f

--- appinventor/blocklyeditor/src/snapshot.js
+++ appinventor/blocklyeditor/src/snapshot.js
@@ -8,11 +8,13 @@
 
 goog.provide('Blockly.Snapshot');
 goog.require('goog.net.XhrIo');
+goog.require('goog.net.XhrIoPool');
 
 //var dataUrl = 'http://msp.cs.uml.edu/api';
 var dataUrl = 'http://localhost:8000';
 
 var xhr = new goog.net.XhrIo();
+var xhrPool = new goog.net.XhrIoPool(); //defaults to max instances = 10
 var idno = 0;
 
 // Listen for completed RPC calls
@@ -77,5 +79,5 @@ Blockly.Snapshot.send = function(eventType) {
 	console.log("Data:\n");
 	console.log(content);
 
-	xhr.send(dataUrl, "POST", content);
+	xhrPool.getObject().send(dataUrl, "POST", content);
 };
